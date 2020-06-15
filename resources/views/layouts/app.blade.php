39117ba97a35a18e41fac4580a0f74d140b47f75
Stefan Burghelea <44903771+StefanBurghelea@users.noreply.github.com>
2020-06-15 11:40:54 +0100

    icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    <link rel="shortcut icon" href="vendor\adminlte\dist\img\logo.png">
+
+
 </head>
 <body>
     <div id="app">
